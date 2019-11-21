f47660b95c1c8ea93533aacd1f58ddde7272ad21
Derrick White <derrickpwhite1@gmail.com>
2019-11-21 15:02:42 -0500

    actally added router (to app.js)

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const cors = require("cors");
 const fetch = require("node-fetch");
-
+const raceRouter = require('./src/routes/Race.router');
 
 const app = express();
 app.use(cors());
+app.use(raceRouter);
 
 fetch("http://dnd5eapi.co/api/ability-scores/2", {
      method: "GET"
